2ebb844993189ac76af51d9808008f51ec75a9f0
Ednaldo Florenço <153869774+EdnaldoBarbo@users.noreply.github.com>
2025-07-25 21:13:16 -0300

    Update HomeController.java

--- src/main/java/com/example/WebApp/HomeController.java
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -1,18 +1,23 @@
 package com.example.WebApp;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
+
     @RequestMapping("/")
     public @ResponseBody String greeting() {
         return "Hello, World!";
     }
+
     @RequestMapping("/ednal")
     public @ResponseBody String greetingednal() {
         return "Hello, Ednaldo!";
     }
-    // Novo endpoint
+
     @GetMapping("/hello")
     public String helloWorld() {
         return "Endpoint final";
